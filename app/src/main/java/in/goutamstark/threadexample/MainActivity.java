a16c92990ba60a9bdc7f48d47dc10bf95e3f3af4
mrgoutam <mrgoutam8.3@gmail.com>
2019-05-12 08:48:35 +0530

    all functionality completed

--- app/src/main/java/in/goutamstark/threadexample/MainActivity.java
+++ app/src/main/java/in/goutamstark/threadexample/MainActivity.java
@@ -1,23 +1,32 @@
 package in.goutamstark.threadexample;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 //https://www.youtube.com/watch?v=QfQE1ayCzf8&list=PLrnPJCHvNZuD52mtV8NvazNYIyIVPVZRa
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity"; //shortcut=> logt
+    private Button buttonStartThread;
+    private Handler mainHandler = new Handler();
+    private volatile boolean stopThread = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonStartThread = findViewById(R.id.button_start_thread);
     }
 
 
 
     public void startThread(View view){
+        stopThread = false;
 
         /*//we will do a heavy operation
         for(int i=0; i<=10; i++){
@@ -40,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void stopThread(View view){
-
+        stopThread = true;
     }
 
     class ExampleThread extends Thread{
@@ -73,9 +82,71 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void run() {
+            if (stopThread){
+                return;
+            }
             //we will do a heavy operation instead in startThread method as it freezes our mainThread
             for(int i=0; i<=seconds; i++){
                 Log.d(TAG, "startThread: "+i); //shortcut => logd
+
+                /*
+                //App will crash if we are not using this inside a handler
+                if (i == 5){
+                    buttonStartThread.setText("50%");
+                }*/
+
+                //case I: works fine
+                /*if (i == 5){
+                    mainHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            buttonStartThread.setText("50%");
+                        }
+                    });
+                }*/
+
+                //case II: crash - see the log
+                /*Handler threadHandler = new Handler();
+                if (i == 5){
+                    threadHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            buttonStartThread.setText("50%");
+                        }
+                    });
+                }*/
+
+                //case III: works fine
+                /*Handler threadHandler = new Handler(Looper.getMainLooper());
+                if (i == 5){
+                    threadHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            buttonStartThread.setText("50%");
+                        }
+                    });
+                }*/
+
+                //case IV: works fine
+                /*if (i == 5){
+                    buttonStartThread.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            buttonStartThread.setText("50%");
+                        }
+                    });
+                }*/
+
+                //case V: works fine
+                if (i == 5){
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            buttonStartThread.setText("50%");
+                        }
+                    });
+                }
+
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
